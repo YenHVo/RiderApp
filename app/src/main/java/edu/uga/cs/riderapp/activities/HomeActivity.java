a4fc583fa4bb01f58bd7cece7bff47b8792eb804
Yen Vo <yenhvo2004@gmail.com>
2025-04-10 13:55:43 -0400

    Structure created, blank files added

--- app/src/main/java/edu/uga/cs/riderapp/activities/HomeActivity.java
+++ app/src/main/java/edu/uga/cs/riderapp/activities/HomeActivity.java
@@ -1,4 +1,4 @@
-package edu.uga.cs.riderapp;
+package edu.uga.cs.riderapp.activities;
 
 import android.os.Bundle;
 
@@ -7,14 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import edu.uga.cs.riderapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
