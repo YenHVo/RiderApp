cc6aa5978600778665e82aff9e5a9227821b29af
Yen Vo <yenhvo2004@gmail.com>
2025-04-18 20:53:35 -0400

    Main Activity code added, AuthActivity created

--- app/src/main/java/edu/uga/cs/riderapp/activities/MainActivity.java
+++ app/src/main/java/edu/uga/cs/riderapp/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.uga.cs.riderapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,9 @@ import edu.uga.cs.riderapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button signUpButton;
+    Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,23 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        signUpButton = findViewById(R.id.sign_up_button);
+        loginButton = findViewById(R.id.login_button);
+
+        signUpButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AuthActivity.class);
+            intent.putExtra("auth_mode", "signup");
+            startActivity(intent);
+            finish();
+        });
+
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AuthActivity.class);
+            intent.putExtra("auth_mode", "login");
+            startActivity(intent);
+            finish();
         });
     }
 }
